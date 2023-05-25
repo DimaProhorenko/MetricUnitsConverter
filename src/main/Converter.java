fcf015566c88741f27dc28ec705777d691334904
DimaProhorenko <dima.study.web@gmail.com>
2023-05-25 23:28:15 +0300

    Add convertKmsToMiles method

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -4,4 +4,8 @@ public abstract class Converter {
     public static double convertMilesToKms(double miles) {
         return miles * 1.60934;
     }
+
+    public static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
+    }
 }
