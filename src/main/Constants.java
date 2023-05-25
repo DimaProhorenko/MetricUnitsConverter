f9a1766c993c0a8444581087223becd29171d037
DimaProhorenko <dima.study.web@gmail.com>
2023-05-25 23:50:29 +0300

    Add Constants Class

---
+++ src/main/Constants.java
@@ -0,0 +1,5 @@
+package main;
+
+public class Constants {
+    public static final double KM_MILES_MULTIPLIER = 1.60934;
+}
--- src/main/Converter.java
+++ src/main/Converter.java
@@ -2,10 +2,10 @@ package main;
 
 public abstract class Converter {
     public static double convertMilesToKms(double miles) {
-        return miles * 1.60934;
+        return miles * Constants.KM_MILES_MULTIPLIER;
     }
 
     public static double convertKmsToMiles(double kms) {
-        return kms / 1.60934;
+        return kms / Constants.KM_MILES_MULTIPLIER;
     }
 }
