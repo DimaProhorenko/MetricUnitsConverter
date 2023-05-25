dfad0d1d38806be663146d32f09224b0f6977fb1
DimaProhorenko <dima.study.web@gmail.com>
2023-05-26 00:17:09 +0300

    Change testConverter method

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,12 +12,10 @@ public class Main {
     private static void testConverter(Random rand) {
         for(int i = 0; i < 10; i++) {
             double km = rand.nextDouble(15);
-            System.out.printf("%.2fkm is %.2f miles\n", km, Converter.convertKmsToMiles(km));
-        }
-        System.out.println();
-        for(int i = 0; i < 10; i++) {
-            double miles = rand.nextDouble(15);
-            System.out.printf("%.2f miles is %.2fkm\n", miles, Converter.convertMilesToKms(miles));
+            double miles = Converter.convertKmsToMiles(km);
+            System.out.printf("%.2fkm is %.2f miles\n", km, miles);
+            System.out.printf("%.2fmiles is %.2f km\n", miles, Converter.convertMilesToKms(miles));
+            System.out.println();
         }
     }
 }
