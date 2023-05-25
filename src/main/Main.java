5245a0c77a571dd8e367482736125172f811ed07
DimaProhorenko <dima.study.web@gmail.com>
2023-05-25 23:39:23 +0300

    Merge, fast-forward

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        System.out.println("Main, Fast Forward");
         System.out.println(Converter.convertMilesToKms(1));
         System.out.println(Converter.convertKmsToMiles(1.6));
     }
